5e271bdbc67e09988e9b92c727706608388ea560
Ranjit Poudel <poudeltilak999@gmail.com>
2026-02-18 16:22:52 +0545

    feat: Schema made for the Unverified user

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -3,3 +3,4 @@ export * from './schemas/cart';
 export * from './schemas/products';
 export * from './schemas/orders';
 export * from './schemas/orderItems';
+export * from './schemas/pending_registrations';
